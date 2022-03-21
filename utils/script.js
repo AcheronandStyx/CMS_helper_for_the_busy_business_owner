ca4036db99128b45504adf0e811191fbc507e8da
AcheronandStyx <matthew.danielson@trojans.dsu.edu>
2022-03-20 23:08:26 -0500

    SQL queries done

--- utils/script.js
+++ utils/script.js
@@ -1,13 +1,25 @@
 const db = require('./connection.js');
 
 const viewAllRoles = function () {
-    return db.query(`SELECT * FROM roles`, function (err, results) {
+    return db.query(`
+    SELECT roles.job_id, roles.title, department.dept_name, roles.salary AS salary
+    FROM roles
+    LEFT JOIN department
+    on roles.department_id = department.dept_id;
+    `, function (err, results) {
         console.log(results);
     })
 };
 
 const viewAllEmployees = function () {
-    return db.query(`SELECT * FROM employees`, function (err, results) {
+    return db.query(`
+    SELECT employees.employee_id, employees.first_name, employees.last_name, roles.title AS title, department.dept_name AS department, roles.salary AS salary, employees.manager_id AS manager
+    FROM employees
+    LEFT JOIN roles
+    on employees.role_id = roles.job_id
+    LEFT JOIN department
+    on roles.department_id = department.dept_id
+    `, function (err, results) {
         console.log(results);
     })
 };
@@ -21,3 +33,26 @@ const viewAllDepartments = function () {
 
 
 module.exports = { viewAllRoles, viewAllEmployees, viewAllDepartments }
+
+    // SQL to return table of role_id, job title, dept name, salary
+
+    /*
+    `
+SELECT roles.job_id, roles.title, department.dept_name, roles.salary AS salary
+FROM roles
+LEFT JOIN department
+on roles.department_id = department.dept_id;
+
+`
+    // SQL for all employees
+    `
+SELECT employees.employee_id, employees.first_name, employees.last_name, roles.title AS title, department.dept_name AS department, roles.salary AS salary, employees.manager_id AS manager
+FROM employees
+LEFT JOIN roles
+on employees.role_id = roles.job_id
+LEFT JOIN department
+on roles.department_id = department.dept_id
+LEFT JOIN employees
+on employees.manager_id = employees.employee_id;
+
+`*/
